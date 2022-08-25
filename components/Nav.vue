2402348830917aa056aa57132bf659df9d60ffe1
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2022-08-25 16:27:20 +0200

    Haeder test
    Ponowy tesst - poprawa animacji oraz screen

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,6 +1,8 @@
 <template>
 <div>
-    <div class="bg-black fixed h-screen w-screen flex md:hidden" ref="m_screen_menu">
+    <div class="bg-black fixed min-h-screen w-screen flex md:hidden overflow-hidden" ref="m_screen_menu">
+        <div class="md:hidden flex absolute  right-10"><button ref="m_menu_button" @click="close"><p class="text-red-800">Zamkij</p></button></div>
+        <div class="md:hidden flex absolute  left-10"><button ref="m_menu_button"><p class="text-red-800">LOGO</p></button></div>
             <div class="absolute flex flex-col bottom-14 gap-4">
                     <NuxtLink to=""><h3 class="text-white" ref="m_page1">Page1</h3></NuxtLink>
                     <NuxtLink to=""><h3 class="text-white" ref="m_page2">Page1</h3></NuxtLink>
@@ -11,8 +13,8 @@
 
 
     <div class="columns-2 w-full mx-auto px-10 flex py-6">
-        <div class=" z-100 text-2xl font-medium absolute text-red-800">LOGO</div>
-        <div class="md:hidden flex absolute  right-10"><button ref="m_menu_button" @click="open()"><p class="text-red-800">{{msg}}</p></button></div>
+        <div class=" z-100 text-2xl font-medium text-red-800">LOGO</div>
+        <div class="md:hidden flex w-full justify-end"><button ref="m_menu_button" @click="open"><p class="text-red-800">{{msg}}</p></button></div>
         <div class="columns-4 w-full md:flex justify-end gap-12 hidden">
             <NuxtLink to=""><h3>Page1</h3></NuxtLink>
             <NuxtLink to=""><h3>Page1</h3></NuxtLink>
@@ -38,7 +40,7 @@ if (process.client) {
   var m_header_open_animation = gsap.timeline({paused: true})
 }
 
-const msg = 'Menu'
+const msg = 'menu'
 
 onMounted(()=>{
 m_header_open_animation.from(m_screen_menu.value,{
@@ -68,9 +70,8 @@ opacity: 0,
 
 })
 
-const open = () => {
-    m_header_open_animation.reversed() ? m_header_open_animation.play() : m_header_open_animation.reverse()
-    }
+const open = () => { m_header_open_animation.play() }
+const close = () => { m_header_open_animation.reverse() }
 
 </script>
 
