f905394d27775a9088de4ae57ba087ce2a02f382
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2023-01-14 19:55:25 +0100

    test v4

--- layouts/scroll.vue
+++ layouts/scroll.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import {onMounted} from 'vue';
 import LocomotiveScroll from 'locomotive-scroll';
-onMounted(() => {
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    smartphone: {
-      smooth: true,
-    },
-  });
-  new ResizeObserver(() => scroll.update()).observe(document.querySelector("[data-scroll-container]"))
-});
+// onMounted(() => {
+//   const scroll = new LocomotiveScroll({
+//     el: document.querySelector('[data-scroll-container]'),
+//     smooth: true,
+//     smartphone: {
+//       smooth: true,
+//     },
+//   });
+//   new ResizeObserver(() => scroll.update()).observe(document.querySelector("[data-scroll-container]"))
+// });
 </script>
   
   <template>
