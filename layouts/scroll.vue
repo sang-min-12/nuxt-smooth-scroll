8a7db9492ed93b887f2e25330fbef0e3c1d9a0cc
Cyprian Waclaw <cyprianwaclaw@gmail.com>
2023-01-14 21:46:00 +0100

    v8

--- layouts/scroll.vue
+++ layouts/scroll.vue
@@ -2,19 +2,19 @@
 import {onMounted} from 'vue';
 import LocomotiveScroll from 'locomotive-scroll';
 onMounted(() => {
-  const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    lerp: 0.03, // Linear Interpolation, 0 > 1 // Try 0.01
-    multiplier: 1.4, // Effect Multiplier
-    reloadOnContextChange: true,
-    touchMultiplier: 2,
-    smoothMobile: 0,
-    smartphone: {
-        smooth: !0,
-    }
-  });
-  new ResizeObserver(() => scroll.update()).observe(document.querySelector("[data-scroll-container]"))
+  // const scroll = new LocomotiveScroll({
+  //   el: document.querySelector('[data-scroll-container]'),
+  //   smooth: true,
+  //   lerp: 0.03, // Linear Interpolation, 0 > 1 // Try 0.01
+  //   multiplier: 1.4, // Effect Multiplier
+  //   reloadOnContextChange: true,
+  //   touchMultiplier: 2,
+  //   smoothMobile: 0,
+  //   smartphone: {
+  //       smooth: !0,
+  //   }
+  // });
+  // new ResizeObserver(() => scroll.update()).observe(document.querySelector("[data-scroll-container]"))
 });
 </script>
   
